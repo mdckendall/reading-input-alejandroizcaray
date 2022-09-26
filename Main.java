c70e6e655d471aec3c3feaf013bbb5eb734fc976
Alejandro Izcaray <113472304+alejandroizcaray@users.noreply.github.com>
2022-09-26 20:43:13 +0000

    read in word

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
   public static void main(String[] args) {
+    
     System.out.println("Enter a word:");
+
+    String name = System.console().readLine();
+
+    System.out.println(name.length());
     
     //Your code will return the length of the entered String
   }
